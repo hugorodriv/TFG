77d1b17e75f40f6e48cd174cf3e4e1b2da0636b5
Hugo <114680022+hugorodriv@users.noreply.github.com>
2025-04-03 13:00:55 +0200

    dropdown menu styling

--- svelte/src/routes/Bottombar.svelte
+++ svelte/src/routes/Bottombar.svelte
@@ -98,10 +98,6 @@
     </div>
 </div>
 
-<!-- Icons -->
-<!-- map: Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2024 Fonticons, Inc. -->
-<!-- rest: https://github.com/themesberg/flowbite-icons -->
-
 <style>
     .active {
         color: black;
--- svelte/src/routes/Navbar.svelte
+++ svelte/src/routes/Navbar.svelte
@@ -3,7 +3,7 @@
     import { onMount } from "svelte";
 
     /**
-     * @type {{ name: String; }}
+     * @type {{ name: String; username: String; }}
      */
     let accountData;
     /**
@@ -38,7 +38,7 @@
     <a href="/" class="text-xl font-bold text-gray-800">--- TFG ---</a>
 
     <div class="relative">
-        <!-- Open dropdown menu -->
+        <!-- Open dropdown button -->
         <button
             id="dropdownBtn"
             on:click={() => {
@@ -55,24 +55,59 @@
             </div>
 
             <span class="text-gray-600 px-1">{accountData?.name}</span>
+            <svg class="w-2.5 h-2.5" fill="none" viewBox="0 0 10 6">
+                <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 1 4 4 4-4"
+                />
+            </svg>
         </button>
 
         <!-- Dropdown menu -->
         <div
             id="dropdown"
-            class="hidden absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 border border-gray-100"
+            class="hidden absolute right-0 bg-white mt-4 w-48 rounded-md shadow-lg py-1 border border-gray-200"
         >
-            <a href="./profile" class="block px-4 py-2 hover:bg-gray-100">
-                Profile
-            </a>
-
-            <a
-                href="./signout/"
-                on:click|preventDefault={() => signOut()}
-                class="block px-4 py-2 hover:bg-gray-100 cursor-pointer"
+            <div class="text-center px-4 py-3 border-b-1 border-gray-200">
+                <div>{accountData?.name}</div>
+                <div class="font-medium truncate">@{accountData?.username}</div>
+            </div>
+            <div
+                class="border-b-1 border-gray-200 py-2 text-sm text-gray-700 dark:text-gray-200"
             >
-                Log out
-            </a>
+                <div>
+                    <a
+                        href="."
+                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >! ! ! !</a
+                    >
+                </div>
+                <div>
+                    <a
+                        href="."
+                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >! ! ! !</a
+                    >
+                </div>
+                <div>
+                    <a
+                        href="/profile"
+                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >Profile Settings</a
+                    >
+                </div>
+            </div>
+            <div class="py-2">
+                <a
+                    href="./signout/"
+                    on:click|preventDefault={() => signOut()}
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >Sign out</a
+                >
+            </div>
         </div>
     </div>
 </nav>
