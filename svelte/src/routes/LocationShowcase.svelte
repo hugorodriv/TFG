d7419fa1f2ab279468de7bccf07d4fe3d56abfe8
Hugo <114680022+hugorodriv@users.noreply.github.com>
2025-04-10 17:03:30 +0200

    location handling

---
+++ svelte/src/routes/LocationShowcase.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { onMount } from "svelte";
+    /**
+     * @type {{ lon: any; lat: any; timestamp: any; } }
+     */
+    export let location;
+</script>
+
+<p>Location granted :)</p>
