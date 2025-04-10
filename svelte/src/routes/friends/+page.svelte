bdf60f50b4418e6955c2cc53588b249550c39c40
Hugo <114680022+hugorodriv@users.noreply.github.com>
2025-04-10 18:17:17 +0200

    add spinners to pages so refreshing doesnt temporarily display wrong image in navbar, among other things

--- svelte/src/routes/friends/+page.svelte
+++ svelte/src/routes/friends/+page.svelte
@@ -5,6 +5,7 @@
     import ProfileCard from "./ProfileCard.svelte";
 
     export let data;
+    let loading = true;
 
     /**
      * @type {{ uuid: String; }}
@@ -12,90 +13,114 @@
     let accountData;
     onMount(() => {
         accountData = JSON.parse(localStorage.getItem("accData") || "{}");
+        loading = false;
     });
 </script>
 
-<Navbar />
-
-<div class="space-y-4 p-4 max-w-md m-auto">
-    {#if data.pending.length > 0}
-        <p class="text-center text-xl">New friend requests</p>
-    {/if}
-
-    <div class="flow-root">
-        <ul role="list" class="">
-            {#each data.pending as p}
-                <ProfileCard
-                    name={p.sender_name}
-                    username={p.sender_username}
-                    img_url={p.sender_img_url}
-                    sender_uuid={p.sender_uuid}
-                    receiver_uuid={null}
-                    acceptButton={true}
-                />
-            {/each}
-        </ul>
-    </div>
-
-    {#if data.sentPending.length > 0}
-        <p class="text-center text-xl">Sent</p>
-    {/if}
-
-    <div class="flow-root">
-        <ul role="list">
-            {#each data.sentPending as p}
-                <ProfileCard
-                    name={p.receiver_name}
-                    username={p.receiver_username}
-                    img_url={p.receiver_img_url}
-                    sender_uuid={null}
-                    receiver_uuid={p.receiver_uuid}
-                />
-            {/each}
-        </ul>
-    </div>
-
-    <p class="text-center text-xl">Friend list</p>
-
-    <a aria-label="searchbar" href="../search/" class="relative">
-        <div
-            class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
+{#if loading}
+    <div class="mt-28 text-center">
+        <svg
+            class="inline w-8 h-8 text-gray-200 animate-spin fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
         >
-            <svg class="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 20 20">
-                <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-            </svg>
-        </div>
-        <input
-            class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
-            placeholder="Name or username"
-        />
-    </a>
-    {#if data.friendList.length > 0}
+            <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+            />
+            <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+            />
+        </svg>
+    </div>
+{:else}
+    <Navbar />
+
+    <div class="space-y-4 p-4 max-w-md m-auto {loading ? 'hidden' : 'shown'}">
+        {#if data.pending.length > 0}
+            <p class="text-center text-xl">New friend requests</p>
+        {/if}
+
         <div class="flow-root">
             <ul role="list" class="">
-                {#each data.friendList as p}
+                {#each data.pending as p}
                     <ProfileCard
-                        name={p.friend_name}
-                        username={p.friend_username}
-                        img_url={p.friend_img_url}
-                        sender_uuid={null}
-                        receiver_uuid={p.friend_uuid}
-                        isFriend={true}
+                        name={p.sender_name}
+                        username={p.sender_username}
+                        img_url={p.sender_img_url}
+                        sender_uuid={p.sender_uuid}
+                        receiver_uuid={null}
+                        acceptButton={true}
                     />
                 {/each}
             </ul>
         </div>
-    {:else}
-        <p class="my-5 text-center text">
-            No friends found! Try looking somebody up :)
-        </p>
-    {/if}
-</div>
 
-<Bottombar />
+        {#if data.sentPending.length > 0}
+            <p class="text-center text-xl">Sent</p>
+        {/if}
+
+        <div class="flow-root">
+            <ul role="list">
+                {#each data.sentPending as p}
+                    <ProfileCard
+                        name={p.receiver_name}
+                        username={p.receiver_username}
+                        img_url={p.receiver_img_url}
+                        sender_uuid={null}
+                        receiver_uuid={p.receiver_uuid}
+                    />
+                {/each}
+            </ul>
+        </div>
+
+        <p class="text-center text-xl">Friend list</p>
+
+        <a aria-label="searchbar" href="../search/" class="relative">
+            <div
+                class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
+            >
+                <svg
+                    class="w-4 h-4 text-gray-500"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                >
+                    <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                </svg>
+            </div>
+            <input
+                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
+                placeholder="Name or username"
+            />
+        </a>
+        {#if data.friendList.length > 0}
+            <div class="flow-root">
+                <ul role="list" class="">
+                    {#each data.friendList as p}
+                        <ProfileCard
+                            name={p.friend_name}
+                            username={p.friend_username}
+                            img_url={p.friend_img_url}
+                            sender_uuid={null}
+                            receiver_uuid={p.friend_uuid}
+                            isFriend={true}
+                        />
+                    {/each}
+                </ul>
+            </div>
+        {:else}
+            <p class="my-5 text-center text">
+                No friends found! Try looking somebody up :)
+            </p>
+        {/if}
+    </div>
+
+    <Bottombar />
+{/if}
