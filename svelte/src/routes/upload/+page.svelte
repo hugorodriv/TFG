f4c61f1d6b9b9f8c59bf358dd0c86bb4269d7499
Hugo <114680022+hugorodriv@users.noreply.github.com>
2025-04-24 13:16:07 +0200

    fix update and enforce client side having location

--- svelte/src/routes/upload/+page.svelte
+++ svelte/src/routes/upload/+page.svelte
@@ -1,14 +1,15 @@
 <script>
+    import { locationStore } from "$lib/stores/location";
     import Bottombar from "../Bottombar.svelte";
     import Navbar from "../Navbar.svelte";
     import { onMount } from "svelte";
 
-    let loading = true;
     let uploading = false;
     let successfulUpload = false;
 
     let compressedFile;
 
+    $: isLocationExpired = locationStore.isExpired();
     /**
      * @type {string}
      */
@@ -37,11 +38,28 @@
      */
     onMount(() => {
         accountData = JSON.parse(localStorage.getItem("accData") || "{}");
-        location = JSON.parse(localStorage.getItem("location") || "null");
-        checkCameraAvailability().then(() => {
-            loading = false;
+        updateLocation().then(() => {
+            location = locationStore.getData();
         });
+        checkCameraAvailability().then(() => {});
     });
+
+    async function updateLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+        } else {
+            console.log("error getting location");
+        }
+    }
+
+    async function success(loc) {
+        // Store basic location data first
+        locationStore.update(loc.coords.latitude, loc.coords.longitude);
+    }
+
+    function error(e) {
+        console.error(e);
+    }
 
     async function checkCameraAvailability() {
         try {
@@ -50,7 +68,7 @@
                 (device) => device.kind === "videoinput",
             );
 
-            if (cameras.length === 0) {
+            if (cameras.length === 1) {
                 camError = "Your device doesnt have a compatible camera";
             }
         } catch (err) {
@@ -126,7 +144,7 @@
         uploading = true;
         //
 
-        if (!location || !compressedFile) {
+        if (!location.lat) {
             return;
         }
         const locationInfo = { lat: location.lat, long: location.lon };
@@ -165,209 +183,157 @@
     }
 </script>
 
-{#if loading}
-    <div class="mt-28 text-center">
-        <svg
-            class="inline w-8 h-8 text-gray-200 animate-spin fill-blue-600"
-            viewBox="0 0 100 101"
-            fill="none"
+<Navbar />
+<div class="space-y-4 p-4 max-w-md m-auto">
+    <p class="text-center text-3xl mt-4">Upload</p>
+    {#if !location?.lat || isLocationExpired}
+        <a
+            href="/"
+            class="block mt-10 text-center w-full p-3 bg-red-100 border border-red-300 text-red-700 rounded-md"
         >
-            <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
+            Failed to get location
+        </a>
+    {/if}
+    {#if camError}
+        <div
+            class="mt-10 text-center w-full p-3 bg-red-100 border border-red-300 text-red-700 rounded-md"
+        >
+            {camError}
+        </div>
+    {:else if compressedFile}
+        <!-- Display picture -->
+        <p>{location.resolved}</p>
+        <div
+            class="relative w-full bg-gray-100 rounded-lg shadow-md overflow-hidden"
+        >
+            <img
+                src={URL.createObjectURL(compressedFile)}
+                alt="Camera preview"
+                class="w-full h-full object-cover"
             />
-            <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-            />
-        </svg>
-    </div>
-{:else}
-    <Navbar />
-    {#if !location}
-        <div class="">
-            <div class="mt-10 flex flex-col items-center pb-10">
+            <button
+                on:click={() => {
+                    if (!successfulUpload) {
+                        compressedFile = null;
+                    }
+                }}
+                class="absolute top-2 right-2 bg-white/80 hover:bg-white p-2 rounded-full shadow-md"
+                aria-label="Reset"
+            >
                 <svg
-                    class="w-20 h-20 text-gray-500"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="currentColor"
+                    class="h-5 w-5 text-gray-700"
+                    fill="none"
                     viewBox="0 0 24 24"
+                    stroke="currentColor"
                 >
                     <path
-                        fill-rule="evenodd"
-                        d="M11.906 1.994a8.002 8.002 0 0 1 8.09 8.421 7.996 7.996 0 0 1-1.297 3.957.996.996 0 0 1-.133.204l-.108.129c-.178.243-.37.477-.573.699l-5.112 6.224a1 1 0 0 1-1.545 0L5.982 15.26l-.002-.002a18.146 18.146 0 0 1-.309-.38l-.133-.163a.999.999 0 0 1-.13-.202 7.995 7.995 0 0 1 6.498-12.518ZM15 9.997a3 3 0 1 1-5.999 0 3 3 0 0 1 5.999 0Z"
-                        clip-rule="evenodd"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
                     />
                 </svg>
-                <p class="my-5 text-xl font-medium text-gray-900">
-                    Location is needed to post
-                </p>
+            </button>
+        </div>
 
-                <a
-                    href="/"
-                    type="button"
-                    class="flex items-center gap-2 px-4 py-2.5 text-sm font-medium text-white bg-green-600 hover:bg-green-700 rounded-lg transition-colors"
+        <textarea
+            bind:value={postText}
+            id="postText"
+            class="min-h-48 my-2 block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Write something about your picture :)"
+            maxlength="2048"
+        ></textarea>
+        {#if !uploading && !successfulUpload}
+            <button
+                on:click={uploadPicture}
+                disabled={!location?.lat}
+                type="button"
+                class="w-full text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                >Upload</button
+            >
+        {:else if !successfulUpload}
+            <div class="text-center">
+                <svg
+                    class="inline w-8 h-8 text-gray-200 animate-spin fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                >
+                    <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                    />
+                    <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                    />
+                </svg>
+            </div>
+        {:else if successfulUpload}
+            <button
+                disabled
+                type="button"
+                class="w-full text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                >Uploaded</button
+            >
+        {/if}
+    {:else}
+        <!-- Display upload button -->
+
+        {#if !location?.lat || isLocationExpired}{:else}
+            <div
+                class="mt-10 text-center w-full p-3 bg-blue-100 border border-blue-300 rounded-md"
+            >
+                {location?.resolved}
+            </div>
+        {/if}
+        <button
+            on:click={() => {
+                inputElement.click();
+            }}
+            class="flex items-center justify-center w-full"
+        >
+            <label
+                for="upload"
+                class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"
+            >
+                <div
+                    class="flex flex-col items-center justify-center pt-5 pb-6"
                 >
                     <svg
-                        class="w-5 h-5"
+                        class="mb-1 w-8 h-8 text-gray-500"
+                        width="24"
+                        height="24"
                         fill="none"
-                        stroke="currentColor"
                         viewBox="0 0 24 24"
                     >
                         <path
-                            stroke-linecap="round"
+                            stroke="currentColor"
                             stroke-linejoin="round"
                             stroke-width="2"
-                            d="M5 13l4 4L19 7"
+                            d="M4 18V8a1 1 0 0 1 1-1h1.5l1.707-1.707A1 1 0 0 1 8.914 5h6.172a1 1 0 0 1 .707.293L17.5 7H19a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1Z"
+                        />
+                        <path
+                            stroke="currentColor"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                         />
                     </svg>
-                    Allow location access
-                </a>
-            </div>
-        </div>
-    {:else}
-        <div
-            class="space-y-4 p-4 max-w-md m-auto {loading ? 'hidden' : 'shown'}"
-        >
-            {#if camError}
-                <div
-                    class="mt-10 text-center w-full p-3 bg-red-100 border border-red-300 text-red-700 rounded-md"
-                >
-                    {camError}
+                    <p class="mb-2 text-sm text-gray-500">
+                        <span class="font-semibold">Take picture</span>
+                    </p>
                 </div>
-            {:else if compressedFile}
-                <!-- Display picture -->
-                <p>{location.resolved}</p>
-                <div
-                    class="relative w-full bg-gray-100 rounded-lg shadow-md overflow-hidden"
-                >
-                    <img
-                        src={URL.createObjectURL(compressedFile)}
-                        alt="Camera preview"
-                        class="w-full h-full object-cover"
-                    />
-                    <button
-                        on:click={() => {
-                            if (!successfulUpload) {
-                                compressedFile = null;
-                            }
-                        }}
-                        class="absolute top-2 right-2 bg-white/80 hover:bg-white p-2 rounded-full shadow-md"
-                        aria-label="Reset"
-                    >
-                        <svg
-                            class="h-5 w-5 text-gray-700"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M6 18L18 6M6 6l12 12"
-                            />
-                        </svg>
-                    </button>
-                </div>
+            </label>
+        </button>
 
-                <textarea
-                    bind:value={postText}
-                    id="postText"
-                    class="min-h-48 my-2 block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                    placeholder="Write something about your picture :)"
-                    maxlength="2048"
-                ></textarea>
-                {#if !uploading && !successfulUpload}
-                    <button
-                        on:click={uploadPicture}
-                        type="button"
-                        class="w-full text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-                        >Upload</button
-                    >
-                {:else if !successfulUpload}
-                    <div class="text-center">
-                        <svg
-                            class="inline w-8 h-8 text-gray-200 animate-spin fill-blue-600"
-                            viewBox="0 0 100 101"
-                            fill="none"
-                        >
-                            <path
-                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                fill="currentColor"
-                            />
-                            <path
-                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                fill="currentFill"
-                            />
-                        </svg>
-                    </div>
-                {:else if successfulUpload}
-                    <button
-                        disabled
-                        type="button"
-                        class="w-full text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-                        >Uploaded</button
-                    >
-                {/if}
-            {:else}
-                <!-- Display upload button -->
-                <p class="text-center text-3xl mt-4">Upload</p>
-
-                <p>{location.resolved}</p>
-                <button
-                    on:click={() => {
-                        inputElement.click();
-                    }}
-                    class="flex items-center justify-center w-full"
-                >
-                    <label
-                        for="upload"
-                        class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"
-                    >
-                        <div
-                            class="flex flex-col items-center justify-center pt-5 pb-6"
-                        >
-                            <svg
-                                class="mb-1 w-8 h-8 text-gray-500"
-                                width="24"
-                                height="24"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M4 18V8a1 1 0 0 1 1-1h1.5l1.707-1.707A1 1 0 0 1 8.914 5h6.172a1 1 0 0 1 .707.293L17.5 7H19a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1Z"
-                                />
-                                <path
-                                    stroke="currentColor"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                                />
-                            </svg>
-                            <p class="mb-2 text-sm text-gray-500">
-                                <span class="font-semibold">Take picture</span>
-                            </p>
-                        </div>
-                    </label>
-                </button>
-
-                <input
-                    bind:this={inputElement}
-                    type="file"
-                    accept="image/*"
-                    capture="environment"
-                    on:change={handleCameraInput}
-                    class="hidden"
-                />
-            {/if}
-        </div>
+        <input
+            bind:this={inputElement}
+            type="file"
+            accept="image/*"
+            capture="environment"
+            on:change={handleCameraInput}
+            class="hidden"
+        />
     {/if}
-    <Bottombar />
-{/if}
+</div>
+<Bottombar />
