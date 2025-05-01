f4b8aba405d8abf643353e10fe0d9d7a99c553e3
Hugo <114680022+hugorodriv@users.noreply.github.com>
2025-05-01 13:23:34 +0200

    show skeleton when loading post

--- svelte/src/routes/PostFeed.svelte
+++ svelte/src/routes/PostFeed.svelte
@@ -1,31 +1,10 @@
 <script>
     import { onMount, onDestroy } from "svelte";
+    import Post from "./Post.svelte";
     export let location;
     $: location;
     $: if (location) {
         loadMorePosts();
-    }
-
-    function getTimeAgo(timestamp) {
-        const now = new Date();
-        const pastDate = new Date(timestamp);
-        const diffSec = Math.floor((now - pastDate) / 1000);
-
-        if (diffSec < 60) {
-            return `${diffSec} second${diffSec !== 1 ? "s" : ""} ago`;
-        }
-        const diffMin = Math.floor(diffSec / 60);
-        if (diffMin < 60) {
-            return `${diffMin} minute${diffMin !== 1 ? "s" : ""} ago`;
-        }
-
-        const diffHr = Math.floor(diffMin / 60);
-        if (diffHr < 24) {
-            return `${diffHr} hour${diffHr !== 1 ? "s" : ""} ago`;
-        }
-
-        const diffDay = Math.floor(diffHr / 24);
-        return `${diffDay} day${diffDay !== 1 ? "s" : ""} ago`;
     }
 
     let accountData;
@@ -108,39 +87,6 @@
             loadMorePosts();
         }
     }
-    function getPostLink(post_uuid) {
-        const user_uuid = accountData?.uuid;
-        const timestamp = Date.now();
-
-        // Combine the data
-        const data = {
-            l: [location?.lat, location?.lon],
-            u: user_uuid,
-            t: timestamp,
-            p: post_uuid,
-        };
-
-        // Convert to base64 encoding
-        const encoded = btoa(JSON.stringify(data));
-
-        // Generate the link
-        return `/post/${encodeURIComponent(encoded)}`;
-    }
-
-    function getPosterPfp(pfp_url, poster_name) {
-        if (pfp_url) return pfp_url;
-
-        const letter = poster_name.slice(0, 1).toUpperCase();
-        const svg = `
-            <svg width="128" height="128" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="64" cy="64" r="64" fill="#007BFF"/>
-                <text x="64" y="64" font-size="64" fill="white" font-family="sans-serif"
-                    text-anchor="middle" dominant-baseline="central">
-                    ${letter}
-                </text>
-            </svg>`;
-        return `data:image/svg+xml;base64,${btoa(svg)}`;
-    }
 </script>
 
 {#if location.lat}
@@ -152,37 +98,7 @@
     </div>
     <ul class="mt-4 grid grid-cols-1 gap-8" on:scroll={handleScroll}>
         {#each localPostsCache as post}
-            <li>
-                <a
-                    href={"../p/" + post.username}
-                    class="w-11/12 py-1 m-auto flex items-center gap-3"
-                >
-                    <img
-                        src={getPosterPfp(post.pfp_url, post.poster_name)}
-                        alt="profile"
-                        class="shadow-lg w-10 h-10 rounded-full"
-                    />
-                    <div>
-                        <p class="font-semibold">{post.poster_name}</p>
-                        <p class="text-sm text-gray-500">
-                            {post.resolved_location}
-                        </p>
-                    </div>
-                    <div class="ml-auto text-sm text-gray-400">
-                        {getTimeAgo(post.created_at)}
-                    </div>
-                </a>
-                <a href={getPostLink(post.post_uuid)}>
-                    <div class="overflow-hidden">
-                        <img
-                            loading="lazy"
-                            src={post.img_url}
-                            alt="Post"
-                            class="text-center m-auto rounded w-11/12 h-full object-cover"
-                        />
-                    </div>
-                </a>
-            </li>
+            <Post {post} {accountData} />
         {/each}
     </ul>
     {#if loadingMorePosts}
