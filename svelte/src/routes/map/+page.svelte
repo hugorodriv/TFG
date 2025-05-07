125e4c025d87a092b0d51fc99d050961e529f831
Hugo <114680022+hugorodriv@users.noreply.github.com>
2025-05-07 21:22:36 +0200

    center map button

--- svelte/src/routes/map/+page.svelte
+++ svelte/src/routes/map/+page.svelte
@@ -226,6 +226,10 @@
                 marker.addTo(map);
             });
     }
+    function centerMap() {
+        if (!location.lat || !location.lon) return;
+        map?.flyTo(new L.LatLng(location?.lat, location?.lon), 15);
+    }
     /**
      * @param {number} deg
      */
@@ -294,5 +298,19 @@
         bind:this={mapContainer}
         class="py-2 mx-auto rounded-lg z-0 h-full max-h-[calc(100vh-100px)] w-full"
     ></div>
+
+    <button
+        on:click={centerMap}
+        aria-label="center map"
+        class="shadow-md rounded-full p-3 bg-gray-100 hover:bg-gray-200 cursor-pointer absolute bottom-24 right-12"
+    >
+        <svg class="text-gray-800 w-8 h-8" viewBox="0 0 512 512">
+            <path
+                stroke-width="1.5"
+                fill="currentColor"
+                d="M256 0c17.7 0 32 14.3 32 32l0 34.7C368.4 80.1 431.9 143.6 445.3 224l34.7 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-34.7 0C431.9 368.4 368.4 431.9 288 445.3l0 34.7c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-34.7C143.6 431.9 80.1 368.4 66.7 288L32 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l34.7 0C80.1 143.6 143.6 80.1 224 66.7L224 32c0-17.7 14.3-32 32-32zM128 256a128 128 0 1 0 256 0 128 128 0 1 0 -256 0zm128-80a80 80 0 1 1 0 160 80 80 0 1 1 0-160z"
+            /></svg
+        >
+    </button>
     <Bottombar mapActive={true} />
 </div>
